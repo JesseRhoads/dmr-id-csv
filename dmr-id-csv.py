c41d228f3164e5f195100230374138734c418bd1
Jesse Rhoads <jesserhoads@gmail.com>
2025-06-07 22:04:50 -0400

    expand ignore

--- dmr-id-csv.py
+++ dmr-id-csv.py
@@ -7,6 +7,10 @@
 # This script is a free way to shrink the data set.
 # This reduces toil with some simple text manipulation.
 
+# NOTE: We make an effort to look up the ITU 3-character name of the Country.
+# There is a map you can override for any that fail the lookup, or it takes the first 3 letters.
+# Sincerest apologies if any countries are using the incorrect abbreviation.
+
 # For the input file, download one of the following:
 # https://radioid.net/static/user.csv
 # https://radioid.net/static/users.json
@@ -14,22 +18,18 @@
 import argparse
 import csv
 import logging
-import os
-import signal
 import pycountry
 import json
-import re
 import sys
 import us
 from collections import defaultdict
 
 # Simple 'what are we' to use for later
-prog_name = "dmrids"
+prog_name = "dmr-id-csv"
 prog_description = "Shrinks the DMR ID DB CSV to fit in smaller memory."
 
 # Set up Logging to stderr/stdout
 log = logging.getLogger()
-
 
 class DataRow:
     def __repr__(self):
@@ -39,7 +39,12 @@ class DataRow:
 
     def compress(self):
         # Combine name into one field and limit to 16 chars
-        name = f"{self.fname} {self.lname}"[0:16]
+        # Sometimes the same name is in both fields. This skips over that condition.
+        if self.lname not in self.fname:
+            raw_name = f"{self.fname} {self.lname}"
+        else:
+            raw_name = self.fname
+        name = f"{raw_name}"[0:16]
         self.name = name.split(" ")
         self.fname = self.name[0]
         if len(self.name) < 2:
@@ -79,6 +84,8 @@ class DataRow:
         log.debug(f"Compressed row {self.radio_id}")
 
     def fix_country(self, raw_country):
+        # If the country lookup fails, here are some overrides for some found in the data.
+        # If we can't find it, then we just take the first 3 characters.
         country_map = {
             "U.S. Virgin Islands": "UVI",
             "British Virgin Islands": "BVI",
@@ -108,10 +115,10 @@ class DataRow:
             "COUNTRY": self.country,
         }
 
-    def __init__(self, radio_id, call, fname, lname, city, state, country):
-        # RT-4D expects RADIO_ID,CALLSIGN,FIRST_NAME,LAST_NAME,CITY,STATE,COUNTRY.
+    def __init__(self, radio_id, callsign, fname, lname, city, state, country):
+        # RADIOID CSV Format contains RADIO_ID,CALLSIGN,FIRST_NAME,LAST_NAME,CITY,STATE,COUNTRY.
         self.radio_id = radio_id
-        self.callsign = call
+        self.callsign = callsign
         self.fname = fname
         self.lname = lname
         self.city = city
@@ -120,7 +127,7 @@ class DataRow:
         self.compress()
 
 
-class DMR:
+class DMRidCSV:
     # We use this function when the thing we want to process is only one line at a time.
     # For cases where the data spans multiple lines, we don't do this, we use a generator further down.
     def parse_json_input(self):
@@ -133,42 +140,43 @@ class DMR:
         # CSV Fields: RADIO_ID,CALLSIGN,FIRST_NAME,LAST_NAME,CITY,STATE,COUNTRY
         data = []
         users = []
-        total = 0
+        self.total = 0
+        skip_header = True
         with open(self.args.my_file) as input:
             for unsanitized_line in input:
-                line = unsanitized_line.strip().rstrip("\r").replace("^M", "")
-                total += 1
+                line = unsanitized_line.strip().rstrip("\r\n")
                 if line == "":
                     continue
-                if total == 1:
+                # Skip the first header line
+                if skip_header:
+                    skip_header = False
                     continue
+                # Stop the loop if we've exceeded the desired line limit
                 if self.args.limit:
-                    if total >= self.args.limit:
+                    if self.total >= self.args.limit:
                         break
                 else:
                     data.append(line)
+                    self.total += 1
+
         # CSV already has the fields, so we can start making DataRows
-        #        row = DataRow(radio_id, callsign, fname, lname, city, state, countr
+        #        row = DataRow(radio_id, callsign, fname, lname, city, state, country
         for line in data:
             log.debug(f"line {line}")
             row = line.split(",")
-            my_row = DataRow(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+            my_row = DataRow(row[0], row[1], row[2].strip(), row[3].strip(), row[4], row[5], row[6])
             log.debug(f"Made row {row}")
             self.seen_countries[my_row.country] += 1
             self.rows.append(my_row.to_dict())
-        log.info(f"Total lines parsed: {total}")
-        log.info(f"Entries by Country: {self.seen_countries}")
 
     def parse_json_data(self, data):
-        total = 0
+        self.total = 0
         for user in data["users"]:
             self.parse_user(user)
-            total += 1
+            self.total += 1
             if self.args.limit:
-                if total >= self.args.limit:
+                if self.total >= self.args.limit:
                     break
-        log.info(f"Total lines parsed: {total}")
-        log.info(f"Countries: {self.seen_countries}")
 
     # Example user object in the JSON:
     #   "fname": "Jesse",
@@ -182,12 +190,8 @@ class DMR:
     #   "state": "Florida",
 
     def parse_user(self, user):
-        fname = user.get("name").strip()
+        fname = user.get("fname").strip()
         lname = user.get("surname").strip()
-        if lname not in fname:
-            name = "f{fname} {lname}"
-        else:
-            name = fname
         country = user.get("country")
         state = user.get("state")
         city = user.get("city")
@@ -228,14 +232,14 @@ def get_parameters():
         "-f",
         "--file",
         dest="my_file",
-        help="File to load",
+        help="File name to load in.",
         required=True,
     )
     parser.add_argument(
         "-w",
         "--write",
         dest="csv_out",
-        help="File to write",
+        help="CSV File to write",
     )
     parser.add_argument(
         "-t",
@@ -243,14 +247,14 @@ def get_parameters():
         dest="file_type",
         choices=["csv", "json"],
         default="json",
-        help="csv or json",
+        help="Indicate if this is a CSV or JSON input file.",
     )
     parser.add_argument(
         "-l",
         "--limit",
         dest="limit",
         type=int,
-        help="Number of records to load",
+        help="Number of records to load. This will limit the output file size.",
     )
 
     return parser.parse_args()
@@ -270,9 +274,9 @@ def main():
 
     log.debug(f"{prog_name} Starting up. Arguments: {cli_args}")
     # Set up a managment object to handle scope more readily.
-    dmr_csv = DMR(cli_args)
+    dmr_csv = DMRidCSV(cli_args)
 
-    # Initialize whatever else here.
+    # Parse the data one way if it's a JSON, another if its CSV.
     if cli_args.file_type == "json":
         data = dmr_csv.parse_json_input()
         csv = dmr_csv.parse_json_data(data)
@@ -281,7 +285,10 @@ def main():
 
     if cli_args.csv_out:
         dmr_csv.write_csv()
-
+  
+    log.info(f"Total lines parsed: {dmr_csv.total}")
+    seen = dict(sorted(dmr_csv.seen_countries.items(), key=lambda item: item[1], reverse=True))
+    log.info(f"Entries by Country: {seen}")
 
 if __name__ == "__main__":
     main()
